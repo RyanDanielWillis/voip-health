ac8435a5104249ccaa0062cfdbd01d83a228a0c4
Data Ryan <132403485+RyanDanielWillis@users.noreply.github.com>
2026-05-02 05:42:34 -0400

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 DB_NAME = 'audit_data.db'
